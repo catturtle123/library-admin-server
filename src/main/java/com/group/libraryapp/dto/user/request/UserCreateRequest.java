3222da4ee888946bb9c5f9b9ff50f3b0f20fa2b7
김준환 <musoyou1085@gmail.com>
2025-06-16 16:24:39 +0900

    test: user 저장 테스트

--- src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/group/libraryapp/dto/user/request/UserCreateRequest.java
@@ -2,6 +2,10 @@ package com.group.libraryapp.dto.user.request;
 
 public class UserCreateRequest {
 
+  public UserCreateRequest(String name, Integer age) {
+    this.name = name;
+  }
+
   private String name;
   private Integer age;
 
