3c7a56d7f50f651e219bc09ed75e02df62bfb594
김준환 <musoyou1085@gmail.com>
2025-06-16 16:10:54 +0900

    Apply .gitignore changes

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -6,6 +6,7 @@ fun main() {
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
     calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -52,15 +53,34 @@ class CalculatorTest {
     fun divideTest() {
 
         // given
-        val calculator = Calculator(6)
+        val calculator = Calculator(5)
 
         // when
-        calculator.divide(3)
+        calculator.divide(2)
 
         // then
         if (calculator.number != 2) {
             throw IllegalArgumentException()
         }
+    }
+
+    fun divideExceptionTest() {
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다.")
+            }
+            // 테스트 성공
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
     }
 
 }
