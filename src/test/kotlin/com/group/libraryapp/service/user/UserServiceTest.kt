a4688d1e4179f432b74669ddbd0cff53d3e750fe
김준환 <musoyou1085@gmail.com>
2025-06-16 16:34:40 +0900

    test: 유저 수정, 삭제 테스트 코드 추가 및 displayName 추가

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -3,8 +3,10 @@ package com.group.libraryapp.service.user
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -21,6 +23,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 저장이 정상 동작 한다.")
     fun saveUserTest() {
         // given
         val request = UserCreateRequest("jamey", null)
@@ -36,6 +39,7 @@ class UserServiceTest @Autowired constructor(
     }
 
     @Test
+    @DisplayName("유저 조회가 정상 동작 한다.")
     fun getUsersTest() {
         // given
         userRepository.saveAll(listOf(
@@ -50,5 +54,33 @@ class UserServiceTest @Autowired constructor(
         assertThat(results).hasSize(2)
         assertThat(results).extracting("name").containsExactlyInAnyOrder("A", "B")
         assertThat(results).extracting("age").containsExactlyInAnyOrder(20, null)
+    }
+
+    @Test
+    @DisplayName("유저 이름 수정이 정상 동작 한다.")
+    fun updateUserNameTest() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        val request = UserUpdateRequest(savedUser.id, "B")
+
+        // when
+        userService.updateUserName(request)
+
+        // then
+        val result = userRepository.findAll()[0]
+        assertThat(result.name).isEqualTo("B")
+    }
+
+    @Test
+    @DisplayName("유저 삭제가 정상 동작 한다.")
+    fun deleteUserTest() {
+        // given
+        userRepository.save(User("A", null))
+
+        // when
+        userService.deleteUser("A")
+
+        // then
+        userRepository.findAll().isEmpty()
     }
 }
